e08aadf1d5cf67e1f6246b9454697c130fa63fdc
RenGuangHui <rguanghui.stupid@gmail.com>
2015-01-06 09:49:22 +0800

    通知可以自动关闭了

--- Notification/notification.js
+++ Notification/notification.js
@@ -3,15 +3,15 @@
  * Author: rguanghui.stupid@gmail.com, https://github.com/rguanghui/
  * Update: 2014.11.12
  * 添加对webkit前缀的支持
- * @param configs {Object}
- * configs.title {String}        消息通知的标题
- * configs.body  {String}        消息通知的内容
- * configs.icon  {String}        消息通知的展示图片
- * configs.tag   {String}        标签，使用一致的tag不会让消息通知一层层的累加而是替换之前tag一样的消息通知
+ * @param config {Object}
+ * config.title {String}        消息通知的标题
+ * config.body  {String}        消息通知的内容
+ * config.icon  {String}        消息通知的展示图片
+ * config.tag   {String}        标签，使用一致的tag不会让消息通知一层层的累加而是替换之前tag一样的消息通知
  * congis.show   {Function}      消息通知展示时的函数，默认不做什么响应
- * configs.click {Function}      点击弹出的消息通知后执行的函数
- * configs.close {Function}      关闭消息通知后执行的函数
- * configs.timeout {Number}      是否自动关闭，以及自动关闭的时间间隔(s)        
+ * config.click {Function}      点击弹出的消息通知后执行的函数
+ * config.close {Function}      关闭消息通知后执行的函数
+ * config.timeout {Number}      是否自动关闭，以及自动关闭的时间间隔(s)        
  */
 
 (function(win, doc, undefined) {
@@ -34,21 +34,21 @@
 
   };
   
-  deskNotification.prototype.init = function(configs) {
+  deskNotification.prototype.init = function(config) {
 
-    configs = configs || {};
+    config = config || {};
 
     if (!this.notification) {
-      configs.nosupport && configs.nosupport();
+      config.nosupport && config.nosupport();
       return this;
     }
 
     var defaults = this.defaults;
 
     // 合并参数
-    for(var i in defaults) defaults.hasOwnProperty(i) && !configs.hasOwnProperty(i) && (configs[i] = defaults[i]);
+    for(var i in defaults) defaults.hasOwnProperty(i) && !config.hasOwnProperty(i) && (config[i] = defaults[i]);
 
-    this.configs = configs;
+    this.config = config;
     this.create();
 
     return this;
@@ -76,16 +76,16 @@
   // 创建桌面通知
   deskNotification.prototype.create = function() {
 
-    var configs = this.configs;
+    var config = this.config;
 
     if (this.isPermission()) {
       this.createNoty();
 
       var notification = this.notification; // 这里要重新获取一下生成的notification
-      notification.onshow = configs.show;
-      notification.onclick = configs.click;
-      notification.onclose = configs.close;
-      notification.onerror = configs.error;
+      notification.onshow = config.show;
+      notification.onclick = config.click;
+      notification.onclose = config.close;
+      notification.onerror = config.error;
     } else {
       this.requestPermission();
     }
@@ -93,25 +93,25 @@
   
   deskNotification.prototype.createNoty = function() {
 
-    var configs = this.configs;
+    var config = this.config;
     
     if (this.isNew) {
-      this.notification = new Notification(this.configs.title, {
-        direction : configs.dir,
-        body      : configs.body,
-        icon      : configs.icon,
-        tag       : configs.tag
+      this.notification = new Notification(this.config.title, {
+        direction : config.dir,
+        body      : config.body,
+        icon      : config.icon,
+        tag       : config.tag
       })
     } else {
       this.notification = webkitNotification.createNotification(
-        configs.icon,
-        configs.title,
-        configs.body
+        config.icon,
+        config.title,
+        config.body
       )
     }
     
-    if (configs.timeout) {
-      setTimeout(this.close.bind(this), configs.timeout * 1000);
+    if (config.timeout) {
+      setTimeout(this.close.bind(this), config.timeout * 1000);
     }
   };
 
